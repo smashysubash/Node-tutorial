d99df4cac7faffc3b5ebd10c22eed913497dc27e
Smashy <rocksubash2003@gmail.com>
2022-08-24 06:06:37 +0530

    node express and morgan exersice

---
+++ node-express/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.use((req, res, next)=>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1>hiii</h1>');
+});
+
+const server = http.createServer(app);
+
+
+
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
