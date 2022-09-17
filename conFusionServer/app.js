4365bfcbebdbba31f29714e913eae8b30630ee89
Smashy <76615324+smashysubash@users.noreply.github.com>
2022-09-17 16:22:20 +0530

    express session part 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var fileStore = require('session-file-store')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,13 +33,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-12345-12345-12345'));
 
-app.use(cookieParser('12345-12345-12345-12345'));
-
+app.use(session({
+  name: 'session-id',
+  secret: '12345-12345-12345-12345',
+  saveUninitialized: false,
+  resave: false,
+  store: new fileStore()
+}));
 
 function auth (req, res, next) {
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -49,8 +58,9 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
-        next(); // authorized
+       // res.cookie('user','admin',{signed: true});
+       req.session.user='admin'; 
+       next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
         res.setHeader('WWW-Authenticate', 'Basic');              
@@ -59,7 +69,7 @@ function auth (req, res, next) {
     }
   }
   else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
           next();
       }
       else {
