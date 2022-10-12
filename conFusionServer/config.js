4f34fa29c15d8c8af8608157be1b5df198336a45
Smashy <76615324+smashysubash@users.noreply.github.com>
2022-10-12 21:02:52 +0530

    oauth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://0.0.0.0:27017/conFusion'
+    'mongoUrl':'mongodb://0.0.0.0:27017/conFusion',
+    'facebook':{
+        clientId: '5530246433733194',
+        clientSecret: 'b998c04dc5576e6c161c4fa0b18a501e'
+    }
 }
